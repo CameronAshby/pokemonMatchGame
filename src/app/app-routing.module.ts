c4ff12fca58db99dcb482a6c56a693369e836a2e
Chad Baxter <chaddudeguy@gmail.com>
2019-01-23 09:27:48 -0700

    add player-info service and setup-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProfilePageComponent} from './players/profile-page/profile-page.component';
 import {WelcomePageComponent} from './welcome-page/welcome-page.component';
-
+import {SetupPageComponent} from './setup-page/setup-page.component';
 const routes: Routes = [
   {
     path: 'welcomePage',
     component: WelcomePageComponent
+  },
+  {
+    path: 'setupPage',
+    component: SetupPageComponent
   },
   {
     path: 'playerProfile',
@@ -14,7 +18,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: 'welcomePage'
+    redirectTo: 'setupPage'
   },
 ];
 
--- src/app/services/playerInfo/player-info.service.ts
+++ src/app/services/playerInfo/player-info.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerInfoService {
 
-  constructor() { }
+  constructor(private af: AngularFirestore) { }
+
+  dataTarget() {
+    return this.af.collection('players').doc('player1');
+  }
 }
