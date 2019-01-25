79a9cc511e1d1f357385f760314b61b2424c4837
Chad Baxter <chaddudeguy@gmail.com>
2019-01-25 08:05:54 -0700

    fix merge conflicts again

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -8,6 +8,7 @@ import {Router} from '@angular/router';
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
+
   constructor(private loginService: LoginServiceService, private router: Router) {
   }
 
