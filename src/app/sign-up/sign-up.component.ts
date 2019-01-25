1b4e735cbedbe20f1c0ca765388b75252858dbc7
Cameron Ashby <c00ashby@gmail.com>
2019-01-25 10:19:54 -0700

    fixed firebase player setup

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginServiceService} from '../services/auth/login-service.service';
 import {Router} from '@angular/router';
+import {PlayerInfoService} from '../services/playerInfo/player-info.service';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {Player} from '../interfaces/player';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,7 +11,13 @@ import {Router} from '@angular/router';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-  constructor(private loginService: LoginServiceService) { }
+
+  constructor(
+    private loginService: LoginServiceService,
+    private router: Router,
+    private playerInfoService: PlayerInfoService,
+    private afs: AngularFirestore
+  ) { }
 
   ngOnInit() {
   }
