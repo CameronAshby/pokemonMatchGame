96a010898b6b0ebe1d7f0a756e3c7dcdb44d7008
Chad Baxter <chaddudeguy@gmail.com>
2019-01-25 07:49:50 -0700

    add basic database support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProfilePageComponent} from './players/profile-page/profile-page.component';
 import {WelcomePageComponent} from './welcome-page/welcome-page.component';
+<<<<<<< HEAD
 import {SetupPageComponent} from './setup-page/setup-page.component';
+=======
+import {SignUpComponent} from './sign-up/sign-up.component';
+
+>>>>>>> 717b4b2dcd8dfca5b6302b03822c99c53bb34817
 const routes: Routes = [
   {
     path: 'welcomePage',
-    component: WelcomePageComponent
+    component: WelcomePageComponent,
+    children: [
+      {
+        path: 'signIn',
+        component: SignUpComponent
+      }
+    ]
   },
   {
     path: 'setupPage',
@@ -18,7 +29,11 @@ const routes: Routes = [
   },
   {
     path: '**',
+<<<<<<< HEAD
     redirectTo: 'setupPage'
+=======
+    redirectTo: 'welcomePage/signIn'
+>>>>>>> 717b4b2dcd8dfca5b6302b03822c99c53bb34817
   },
 ];
 
