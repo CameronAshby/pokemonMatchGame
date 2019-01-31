6dfbf4da166670ec50bdaaa1597b7e21b76de6b5
Ethan Choi <kingofkoreankids@gmail.com>
2019-01-31 09:44:20 -0700

    idk

--- src/app/gamepage/gamepage.component.ts
+++ src/app/gamepage/gamepage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PokemonService} from '../services/apiService/apistuff.service';
 import {Card} from '../interfaces/card';
 import {PlayerInfoService} from '../services/playerInfo/player-info.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-gamepage',
@@ -16,7 +17,11 @@ export class GamepageComponent implements OnInit {
   matchArray: Card[] = [];
   matchIndexArray: number[] = [];
 
-  constructor(private pokemonservice: PokemonService, private playerInfoService: PlayerInfoService) {
+  constructor(
+    private pokemonservice: PokemonService,
+    private playerInfoService: PlayerInfoService,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
@@ -24,6 +29,7 @@ export class GamepageComponent implements OnInit {
       .then(data => {
           this.pokemonservice.pokemonArray = data;
           this.buildCards();
+          this.randomizeCards();
         }
       );
   }
@@ -35,7 +41,8 @@ export class GamepageComponent implements OnInit {
         cardId: this.pokemonservice.pokemonArray[this.randomCardIndex].id + '',
         image: this.pokemonservice.pokemonArray[this.randomCardIndex].imageUrl,
         matchId: i+1,
-        clicked: false
+        clicked: false,
+        matched: false
       };
     }
 
@@ -44,14 +51,12 @@ export class GamepageComponent implements OnInit {
         cardId: this.cardsArray[i-this.playerInfoService.gameInfo.matchesCount].cardId,
         image: this.cardsArray[i-this.playerInfoService.gameInfo.matchesCount].image,
         matchId: this.cardsArray[i-this.playerInfoService.gameInfo.matchesCount].matchId,
-        clicked: false
+        clicked: false,
+        matched: false
       }
     }
   }
 
-  async pullApi() {
-    await this.pokemonservice.getPokemon();
-  }
   getRandomCard() {
     this.randomCardIndex = Math.floor((Math.random() * 999));
   }
@@ -59,23 +64,32 @@ export class GamepageComponent implements OnInit {
   toggleClicked(index: number, playerCard: Card) {
     this.cardsArray[index].clicked = true;
     this.matchArray.push(playerCard);
-    setTimeout(()=>{if(this.matchArray.length == 2) {
-      this.checkMatch();
-    }}, 4000);
+    this.matchIndexArray.push(index);
+    setTimeout(()=>{
+      if(this.matchArray.length == 2) {
+        this.checkMatch();
+      }}, 4000);
   }
 
   checkMatch() {
     if(this.matchArray[0].matchId == this.matchArray[1].matchId || this.matchArray[0].cardId == this.matchArray[1].cardId) {
       console.log('Match Found!');
 
+      this.cardsArray[this.matchIndexArray[0]].matched = true;
+      this.cardsArray[this.matchIndexArray[1]].matched = true;
+
       this.matchArray = [];
       this.matchIndexArray = [];
+
+      this.playerInfoService.gameInfo.matchesCount -= 1;
     }
     else {
       console.log('No Match!');
       this.matchArray[0].clicked = false;
       this.matchArray[1].clicked = false;
+
       this.matchArray = [];
+      this.matchIndexArray = [];
     }
   }
 
@@ -84,4 +98,18 @@ export class GamepageComponent implements OnInit {
       this.cardsArray[i].clicked = true;
     }
   }
+  randomizeCards(){
+    for (let i = 0; i < this.cardsArray.length; i++){
+      let ran = Math.floor(Math.random() * this.cardsArray.length);
+      let temp = this.cardsArray[ran];
+      this.cardsArray[ran] = this.cardsArray[i];
+      this.cardsArray[i] = temp;
+    }
+  }
+
+  viewStats() {
+    this.router.navigate(['statsPage']);
+  }
 }
+
+
